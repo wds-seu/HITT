f95416b8b80f7b2e69ad4e2ab22eefbb64ab101a
penngao <525944558@qq.com>
2019-09-09 11:54:44 +0800

    Add fastText model

---
+++ fastText/config.py
@@ -0,0 +1,12 @@
+# config.py
+
+
+class Config(object):
+    embed_size = 300
+    hidden_size = 10
+    output_size = 2
+    max_epochs = 20
+    lr = 0.1
+    batch_size = 128
+    max_sen_len = 20
+    context_flag = 0
